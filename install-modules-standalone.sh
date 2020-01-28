84b05c49d9315248d481e8dca28175e55be73eb2
Emanuele Ballarin <emanuele@ballarin.cc>
2020-01-28 22:45:42 +0100

    Update

--- install-modules-standalone.sh
+++ install-modules-standalone.sh
@@ -11,6 +11,20 @@
 ## MIT License
 ##
 
+####################
+## Set some flags ##
+####################
+unset CXXFLAGS
+unset CFFLAGS
+unset CFLAGS
+unset FFLAGS
+unset LDFLAGS
+export CXXFLAGS="-g -O3 -feliminate-unused-debug-types -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=32 -Wformat -Wformat-security -m64 -fasynchronous-unwind-tables -Wp,-D_REENTRANT -ftree-loop-distribute-patterns -Wl,-z -Wl,now -Wl,-z -Wl,relro -fno-semantic-interposition -ffat-lto-objects -fno-trapping-math -Wl,-sort-common -Wl,--enable-new-dtags -fno-plt -march=native -fvisibility-inlines-hidden -Wl,--enable-new-dtags"
+export CFFLAGS="-g -O3 -feliminate-unused-debug-types -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=32 -m64 -fasynchronous-unwind-tables -Wp,-D_REENTRANT -ftree-loop-distribute-patterns -Wl,-z -Wl,now -Wl,-z -Wl,relro -malign-data=abi -fno-semantic-interposition -ftree-vectorize -ftree-loop-vectorize -Wl,-sort-common -Wl,--enable-new-dtags -fno-plt -march=native"
+export CFLAGS="-g -O3 -feliminate-unused-debug-types -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=32 -Wformat -Wformat-security -m64 -fasynchronous-unwind-tables -Wp,-D_REENTRANT -ftree-loop-distribute-patterns -Wl,-z -Wl,now -Wl,-z -Wl,relro -fno-semantic-interposition -ffat-lto-objects -fno-trapping-math -Wl,-sort-common -Wl,--enable-new-dtags -fno-plt -march=native"
+export FFLAGS="-g -O3 -feliminate-unused-debug-types -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=32 -m64 -fasynchronous-unwind-tables -Wp,-D_REENTRANT -ftree-loop-distribute-patterns -Wl,-z -Wl,now -Wl,-z -Wl,relro -malign-data=abi -fno-semantic-interposition -ftree-vectorize -ftree-loop-vectorize -Wl,--enable-new-dtags -march=native"
+export LDFLAGS="-Wl,-O3,--sort-common,--as-needed,-z,relro,-z,now"
+
 ##################
 ## Get packages ##
 ##################
@@ -32,7 +46,7 @@ cd "$CLEARERMANJARO_TMPDIR"
 
 #git clone --recursive https://github.com/emaballarin/clearer-manjaro-kernel.git
 git clone --recursive https://github.com/emaballarin/clearer-manjaro-kernel-nvidia.git
-git clone --recursive https://gitlab.manjaro.org/packages/extra/linux55-extramodules/acpi_call.git
+git clone --recursive https://github.com/emaballarin/clearer-manjaro-kernel-acpi-call.git
 git clone --recursive https://gitlab.manjaro.org/packages/extra/linux55-extramodules/virtualbox-modules.git
 git clone --recursive https://gitlab.manjaro.org/packages/extra/linux55-extramodules/spl_zfs.git
 
@@ -40,13 +54,13 @@ git clone --recursive https://gitlab.manjaro.org/packages/extra/linux55-extramod
 ## Auto-reconfigure packages ##
 ###############################
 
-# acpi_call
-cd "$CLEARERMANJARO_TMPDIR/acpi_call"
-mv ./acpi_call.install ./acpi_call-CLEARER.install
-sed -i "s/_linuxprefix=.*/_linuxprefix=linux55-CLEARER/g" ./PKGBUILD
-sed -i "s/_extramodules=.*/_extramodules=extramodules-5.5-CLEARER/g" ./PKGBUILD
-sed -i "s/install=\$_pkgname\.install.*/install=acpi_call-CLEARER\.install/g" ./PKGBUILD
-sed -i "s/acpi_call\.install\"/acpi_call-CLEARER\.install\"/g" ./PKGBUILD
+## # acpi_call
+## cd "$CLEARERMANJARO_TMPDIR/acpi_call"
+## mv ./acpi_call.install ./acpi_call-CLEARER.install
+## sed -i "s/_linuxprefix=.*/_linuxprefix=linux55-CLEARER/g" ./PKGBUILD
+## sed -i "s/_extramodules=.*/_extramodules=extramodules-5.5-CLEARER/g" ./PKGBUILD
+## sed -i "s/install=\$_pkgname\.install.*/install=acpi_call-CLEARER\.install/g" ./PKGBUILD
+## sed -i "s/acpi_call\.install\"/acpi_call-CLEARER\.install\"/g" ./PKGBUILD
 
 
 # SPL/ZFS
